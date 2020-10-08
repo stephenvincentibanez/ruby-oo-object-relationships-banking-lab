965a2d773e24b1eb3e1b5962e5cd4c80908d5b4a
Stephen Ibanez <sibanez93@Stephens-MacBook-Pro-3.local>
2020-10-08 10:30:55 -0700

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,19 @@
+require_relative "./bank_account.rb"
 class Transfer
-  # your code here
+
+attr_reader :sender, :receiver
+attr_accessor :status, :amount 
+
+def initialize(sender, receiver, amount, status="pending")
+  @sender = sender 
+  @receiver = receiver 
+  @status = status
+  @amount = amount 
+end 
+
+def valid?
+  @sender.valid?
+  @receiver.valid?
+end 
+
 end
